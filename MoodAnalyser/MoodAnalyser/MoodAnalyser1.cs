5d638dd62ff886e76e07e5b5f2cd3cdf2bea4bc1
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-14 13:39:34 +0530

    [shweta]Refactor analyser using constructor

--- MoodAnalyser/MoodAnalyser/MoodAnalyser1.cs
+++ MoodAnalyser/MoodAnalyser/MoodAnalyser1.cs
@@ -7,17 +7,25 @@ using System.Threading.Tasks;
 namespace MoodAnalyser
 {
     public class MoodAnalyser1
-    {        
-            public string Mood(string message)
+    {
+        public string message;
+        //parameterized constructor
+        public MoodAnalyser1(string message)
+        {
+            this.message = message;
+        }
+        //Method to return the type of Mood
+        public string Mood()
+        {
+            if (message.ToLower().Contains("happy"))
             {
-                if (message.ToLower().Contains("happy"))
-                {
-                    return "happy";
-                }
-                else
-                {
-                    return "sad";
-                }
+                return "happy";
+            }
+            else
+            {
+                return "sad";
             }
         }
+
+    }
 }
--- MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
@@ -6,34 +6,32 @@ namespace MoodAnalyserTest
 {
     [TestClass]
     public class UnitTest1
-    {       
-            [TestMethod]
-            [TestCategory("Happy Mood")]
-            public void GivenMessageShouldReturnHappy()
-            {
-                ///Follow AAA strategy
-                ///Arrange , Act and in last Assert
-                MoodAnalyser1 mood = new MoodAnalyser1();
-                string message = "I am in Happy Mood";
-                string excepted = "happy";
-                var actual = mood.Mood(message);
-                Assert.AreEqual(excepted, actual);
+    {
+        [TestMethod]
+        [TestCategory("Happy Mood")]
+        public void GivenMessageShouldReturnHappy()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            MoodAnalyser1 mood = new MoodAnalyser1("I am in Happy Mood");
+            string excepted = "happy";
+            var actual = mood.Mood();
+            Assert.AreEqual(excepted, actual);
             Console.WriteLine(actual);
-            }
-            [TestMethod]
-            [TestCategory("SAD Mood")]
-            public void GivenMessageShouldReturnSad()
-            {
-                ///Follow AAA strategy
-                ///Arrange , Act and in last Assert
-                MoodAnalyser1 mood = new MoodAnalyser1();
-                string message = "I am in SAD Mood";
-                string excepted = "sad";
-                var actual = mood.Mood(message);
-                Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("SAD Mood")]
+        public void GivenMessageShouldReturnSad()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            MoodAnalyser1 mood = new MoodAnalyser1("I am in SAD Mood");
+            string excepted = "sad";
+            var actual = mood.Mood();
+            Assert.AreEqual(excepted, actual);
             Console.WriteLine(actual);
-            }
         }
     }
+}
     
 
